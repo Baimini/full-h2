a2622d05d1e6b182d7250ae7888bc05b64bff2c8
Baimini <98502673+Baimini@users.noreply.github.com>
2024-12-27 15:50:23 +0800

    Backend finished

---
+++ backend/src/main/java/com/example/backend/service/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.model.Product;
+import com.example.backend.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    };
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    };
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    };
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    };
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    };
+}
